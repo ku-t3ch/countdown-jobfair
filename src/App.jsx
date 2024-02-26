c9a77812bbd2c32a71b110d5a632135d73c9accd
parwer <55444278+parwer@users.noreply.github.com>
2024-02-26 22:41:55 +0700

    จัดโครงสร้างใหม่

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,34 @@ import { CountDown } from './compontns/CountDown'
 
 function App() {
     return (
-        <>
-            <div className="flex justify-center items-center my-8">
+        <div className="h-screen flex flex-col justify-center padding-20">
+            
+                
+            <div className="flex justify-center">
                 <a href="https://www.ku.ac.th/th/news1/view/KU-JOB-FAIR-2024" target="_blank">
                     <img src={KULogo} className="logo" alt="KU logo" style={{ height: 100, width: 350 }} />
                 </a>
             </div>
 
-            <div className="flex justify-center items-center my-20">
+            <div className="my-20">
                 <CountDown />
             </div>
 
-            {/* <h2 className="ml-2">d  h  m  s</h2> */}
 
-            <h2 className="text-3xl font-bold text-center mb-4">KU Job Fair 2024</h2>
 
-            <div className="">
-                <p className="text-center">
+            <div className="text-center">
+                <h2 className="text-3xl font-bold text-center mb-4">KU Job Fair 2024</h2>
+                <p className="">
                     มหาวิทยาลัยเกษตรศาสตร์ กำหนดจัดโครงการ KU Job Fair & Education 2024
                 </p>
-                <p className="text-center mt-50">
+                <p className="mt-50">
                     ในวันที่ 29 กุมภาพันธ์ - 1 มีนาคม 2567 ระหว่างเวลา 10.00 - 17.00 น.
                 </p>
-                <p className="text-center mt-50">
+                <p className="mt-50">
                     ณ ศูนย์เรียนรวมมหาวิทยาลัยเกษตรศาสตร์ บางเขน
                 </p>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/compontns/TimeRanderer.jsx
+++ src/compontns/TimeRanderer.jsx
@@ -1,13 +1,22 @@
 import { Completionist } from "./Completionist"
+import { zeroPad } from 'react-countdown';
 
 export const TimeRenderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
         return <Completionist />;
     }else{
         return (
-            <h1>
-                {days}:{hours}:{minutes}:{seconds}
-            </h1>
+            <div className="grid grid-cols-4 gap-4 justify-items-center">
+                <div className="text-8xl">{zeroPad(days)}</div>
+                <div className="text-8xl">{zeroPad(hours)}</div>
+                <div className="text-8xl">{zeroPad(minutes)}</div>
+                <div className="text-8xl">{zeroPad(seconds)}</div>
+
+                <div className="text-3xl">days</div>
+                <div className="text-3xl">hours</div>
+                <div className="text-3xl">minutes</div>
+                <div className="text-3xl">seconds</div>
+            </div>
         )
     }
 }
